062ccb39c502c5cbd22bbeb2e00674a938aedf81
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2025-12-13 19:03:49 +0300

    feat (Find closest target point to screen)

--- Systems/PlayerControllerSystem/Combat/Targeting/Targeter.cs
+++ Systems/PlayerControllerSystem/Combat/Targeting/Targeter.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class Targeter : MonoBehaviour
@@ -7,6 +8,15 @@ public class Targeter : MonoBehaviour
     public List<Target> targets = new List<Target>();
 
     public Target SelectedTarget { get; private set; }
+    [SerializeField] private CinemachineTargetGroup cmTargetGroup;
+    [SerializeField] private float targetWeight = 1f;
+    [SerializeField] private float targetRadius = 1f;
+    [SerializeField] private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -14,6 +24,7 @@ public class Targeter : MonoBehaviour
         if (!targets.Contains(target))
         {
             targets.Add(target);
+            target.OnTargetDestroyed += RemoveTarget;
         }
     }
 
@@ -22,16 +33,74 @@ public class Targeter : MonoBehaviour
         if (!other.TryGetComponent<Target>(out Target target)) return;
         if (targets.Contains(target))
         {
-            targets.Remove(target);
+            RemoveTarget(target);
         }
     }
 
     public bool SelectTarget()
     {
         if (targets.Count == 0) return false;
-        SelectedTarget = targets[0];
+
+        Target closestTarget = null;
+        float closestTargetDistance = Mathf.Infinity;
+        foreach (var target in targets)
+        {
+            Vector2 viewPos = mainCamera.WorldToViewportPoint(target.transform.position);
+
+            if (viewPos is not { x: > 0 and < 1, y: > 0 and < 1 })
+            {
+                continue;
+            }
+
+            Vector2 targetToCenter = viewPos - new Vector2(0.5f, 0.5f); // center of screen thats why 0,5f,0,5f
+            if (targetToCenter.sqrMagnitude < closestTargetDistance)
+            {
+                closestTarget = target;
+                closestTargetDistance = targetToCenter.sqrMagnitude;
+            }
+        }
+
+        if (closestTarget == null)
+        {
+            return false;
+        }
+
+        SelectedTarget = closestTarget;
+        if (cmTargetGroup == null)
+        {
+            Debug.LogError("Targeter has no CineMachine Target Group! You need to assing it");
+            return false;
+        }
+
+        cmTargetGroup.AddMember(SelectedTarget?.transform, targetWeight, targetRadius);
+
         return true;
     }
 
-    public void DeselectTarget() => SelectedTarget = null;
+    public void DeselectTarget()
+    {
+        if (SelectedTarget == null) return;
+
+        if (cmTargetGroup == null)
+        {
+            Debug.LogError("Targeter has no CineMachine Target Group! You need to assing it");
+            return;
+        }
+
+        cmTargetGroup.RemoveMember(SelectedTarget?.transform);
+        SelectedTarget = null;
+    }
+
+    private void RemoveTarget(Target target)
+    {
+        if (!targets.Contains(target)) return;
+        if (SelectedTarget == target)
+        {
+            cmTargetGroup.RemoveMember(SelectedTarget?.transform);
+            SelectedTarget = null;
+        }
+
+        target.OnTargetDestroyed -= RemoveTarget;
+        targets.Remove(target);
+    }
 }

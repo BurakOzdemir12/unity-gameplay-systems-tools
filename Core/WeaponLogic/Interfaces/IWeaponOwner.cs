917d7c4ccbc1517b80fcb6f54de3eeda1ca50c73
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2025-12-18 19:14:39 +0300

    refactor (weapon logic)

---
+++ Core/WeaponLogic/Interfaces/IWeaponOwner.cs
@@ -0,0 +1,12 @@
+﻿using _Project.Systems.Core.GravityForce;
+using UnityEngine;
+
+namespace _Project.Systems.Core.WeaponLogic.Interfaces
+{
+    public interface IWeaponOwner
+    {
+        CharacterController Controller { get; }
+        ForceReceiver ForceReceiver { get; }
+        Transform Transform { get; }
+    }
+}
--- Core/WeaponLogic/WeaponLogic.cs
+++ Core/WeaponLogic/WeaponLogic.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using _Project.Systems.Core.GravityForce.Interfaces;
 using _Project.Systems.Core.Health;
 using _Project.Systems.Core.Health.Interfaces;
 using UnityEngine;
@@ -7,36 +9,80 @@ namespace _Project.Systems.Core.WeaponLogic
 {
     public class WeaponLogic : MonoBehaviour
     {
-        [SerializeField] private Collider playerOwnCollider;
-        [SerializeField] private float attackDamage;
-        private readonly HashSet<Collider> damagedColliders = new HashSet<Collider>();
+        [SerializeField] private Collider characterOwnCollider;
+        private readonly HashSet<Collider> hitColliders = new HashSet<Collider>();
+        
+        private bool active;
+        private float currentDamage;
+        private float currentKnockbackForce;
 
         private float currentAttackMultiplier = 1f;
 
         private void OnEnable()
         {
-            damagedColliders.Clear();
+            hitColliders.Clear();
         }
 
+        private void OnDisable()
+        {
+            hitColliders.Clear();
+            active = false;
+        }
+
+        public void Initialize(Collider ownerCollider)
+        {
+            characterOwnCollider = ownerCollider;
+        }
+
+        public void BeginAttack(float finalDamage, float finalKnockbackForce)
+        {
+            currentDamage = finalDamage;
+            currentKnockbackForce = finalKnockbackForce;
+            
+            hitColliders.Clear();
+            active = true;
+            
+            gameObject.SetActive(true);
+        }
+
+        public void EndAttack()
+        {
+            active = false;
+            hitColliders.Clear();
+
+            gameObject.SetActive(false);
+        }
+        
         private void OnTriggerEnter(Collider other)
         {
-            if (other == playerOwnCollider) return;
+            if (!active) return;
+
+            if (other == characterOwnCollider) return;
+            if (!hitColliders.Add(other)) return;
 
             if (other.TryGetComponent<IDamageable>(out var damageable))
             {
                 if (damageable == null) return;
 
-                if (!damagedColliders.Add(other)) return;
 
-                float finalDamage = attackDamage * currentAttackMultiplier;
-                damageable.ApplyDamage(finalDamage);
-                Debug.Log("damaged");
+                damageable.ApplyDamage(currentDamage);
+            }
+
+            if (other.TryGetComponent<IKnockable>(out var knockable))
+            {
+                if (knockable == null) return;
+                Vector3 dir = (other.transform.position - transform.root.position);
+                // Vector3 dir = (other.transform.position - characterOwnCollider.transform.position);
+                dir.y = 0f;
+                knockable.ApplyKnockback(currentKnockbackForce, dir);
             }
         }
 
-        public void SetAttackAttributes(float damageMultiplier)
-        {
-            currentAttackMultiplier = damageMultiplier;
-        }
+        // public void SetAttackAttributes(float damageMultiplier, float knockbackForce, float attackDamage)
+        // {
+        //     this.currentAttackMultiplier = damageMultiplier;
+        //     this.currentKnockbackForce = knockbackForce;
+        //     this.currentDamage = attackDamage;
+        // }
     }
 }

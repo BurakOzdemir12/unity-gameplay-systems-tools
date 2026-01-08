93507112b4df1bbd90664a4966a3cf138f7944f3
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2026-01-08 18:06:06 +0300

    feat: Event Bus System

---
+++ _Core/EventBus/EventBus.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using _Project.Systems._Core.EventBus.Interfaces;
+using UnityEngine;
+
+namespace _Project.Systems._Core.EventBus
+{
+    public static class EventBus<T> where T : IEvent
+    {
+        private static readonly HashSet<IEventBinding<T>> bindings = new HashSet<IEventBinding<T>>();
+
+        public static void Subscribe(EventBinding<T> binding) => bindings.Add(binding);
+        public static void Unsubscribe(EventBinding<T> binding) => bindings.Remove(binding);
+
+
+        public static void Publish(T @event)
+        {
+            foreach (var binding in bindings)
+            {
+                binding.OnEvent.Invoke(@event);
+                binding.OnEventNoArgs.Invoke();
+            }
+        }
+
+        private static void Clear()
+        {
+            bindings.Clear();
+            Debug.Log("Cleared EventBus" + typeof(T).Name + "Bindings");
+        }
+    }
+}
---
+++ _Core/EventBus/Interfaces/IEvent.cs
@@ -0,0 +1,6 @@
+﻿namespace _Project.Systems._Core.EventBus.Interfaces
+{
+    public interface IEvent
+    {
+    }
+}

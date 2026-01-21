471c33839fb2bc967f05607123681683304eb8a6
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2026-01-21 22:56:04 +0300

    feat: Player and Enemy Hurtbox logic

---
+++ _Core/HitboxLogic/EnemyHurtbox.cs
@@ -0,0 +1,9 @@
+﻿namespace _Project.Systems._Core.HitboxLogic
+{
+    public class EnemyHurtbox : HurtboxBase
+    {
+        protected override void OnHitApplied(float finalDamage)
+        {
+        }
+    }
+}
---
+++ _Core/HitboxLogic/PlayerHurtbox.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Project.Systems._Core.HitboxLogic
+{
+    public class PlayerHurtbox:HurtboxBase
+    {
+        protected override void OnHitApplied(float finalDamage)
+        {
+        }
+    }
+}

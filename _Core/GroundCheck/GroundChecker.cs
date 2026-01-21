92456bc9f66a7561a8ec216817972d8a458918b3
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2026-01-21 22:57:34 +0300

    refactor: Adding each foot Sphere cast for ground Check

--- _Core/GroundCheck/GroundChecker.cs
+++ _Core/GroundCheck/GroundChecker.cs
@@ -9,6 +9,8 @@ namespace _Project.Systems._Core.GroundCheck
         [SerializeField] private float groundCheckRadius = 0.3f;
         [SerializeField] private LayerMask groundLayer;
         [SerializeField] private Vector3 offset = new Vector3(0, 0.1f, 0);
+        [SerializeField] private Vector3 leftOffset = new Vector3(0, 0.1f, 0);
+        [SerializeField] private Vector3 rightOffset = new Vector3(0, 0.1f, 0);
 
         [Header("State")] [SerializeField] private bool isGrounded;
         [SerializeField] private float distanceToGround;
@@ -16,6 +18,11 @@ namespace _Project.Systems._Core.GroundCheck
 
         [Header("Capsule Or Sphere")] [SerializeField]
         private bool useCapsule;
+
+        [Header("Foot Bones")] [SerializeField]
+        private Transform leftFoot;
+
+        [SerializeField] private Transform rightFoot;
 
         public bool IsGrounded => isGrounded;
         public float DistanceToGround => distanceToGround;
@@ -60,8 +67,14 @@ namespace _Project.Systems._Core.GroundCheck
                 }
                 case false:
                 {
+                    Vector3 leftOrigin = leftFoot.position + leftOffset;
+                    Vector3 rightOrigin = rightFoot.position + rightOffset;
                     // Using SphereCast for better coverage than Raycast
-                    if (Physics.SphereCast(origin, groundCheckRadius, Vector3.down, out hit, groundCheckDistance,
+
+                    if (Physics.SphereCast(leftOrigin, groundCheckRadius, Vector3.down, out hit, groundCheckDistance,
+                            groundLayer)
+                        ||
+                        Physics.SphereCast(rightOrigin, groundCheckRadius, Vector3.down, out hit, groundCheckDistance,
                             groundLayer))
                     {
                         if (Vector3.Angle(hit.normal, Vector3.up) > maxSlopeAngle)
@@ -94,14 +107,16 @@ namespace _Project.Systems._Core.GroundCheck
             float distanceToPoints = (offset.y / 2f) - groundCheckRadius;
 
             Vector3 center = transform.position + new Vector3(0, 1, 0);
-            p1 = center + Vector3.up * distanceToPoints; // Üst Merkez
-            p2 = center - Vector3.up * distanceToPoints; // Alt Merkez
+            p1 = center + Vector3.up * distanceToPoints;
+            p2 = center - Vector3.up * distanceToPoints;
         }
 
         private void OnDrawGizmosSelected()
         {
             Gizmos.color = isGrounded ? Color.green : Color.red;
-            Vector3 origin = transform.position + offset;
+            // Vector3 origin = transform.position + offset;
+            Vector3 leftOrigin = leftFoot.position + leftOffset;
+            Vector3 rightOrigin = rightFoot.position + rightOffset;
             if (useCapsule)
             {
                 Vector3 point1 = transform.position;
@@ -119,10 +134,14 @@ namespace _Project.Systems._Core.GroundCheck
             }
             else
             {
-                Gizmos.DrawSphere(origin, groundCheckRadius);
+                // Gizmos.DrawSphere(origin, groundCheckRadius);
+                Gizmos.DrawSphere(leftOrigin, groundCheckRadius);
+                Gizmos.DrawSphere(rightOrigin, groundCheckRadius);
             }
 
-            Gizmos.DrawLine(origin, origin + Vector3.down * (groundCheckDistance + groundCheckRadius));
+            // Gizmos.DrawLine(origin, origin + Vector3.down * (groundCheckDistance + groundCheckRadius));
+            Gizmos.DrawLine(leftOrigin, leftOrigin + Vector3.down * (groundCheckDistance + groundCheckRadius));
+            Gizmos.DrawLine(rightOrigin, rightOrigin + Vector3.down * (groundCheckDistance + groundCheckRadius));
         }
     }
 }

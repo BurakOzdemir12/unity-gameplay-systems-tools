7a8a7f361d9dec253e2d7a3f3f27f1d5090af44f
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2026-01-28 14:38:18 +0300

    feat: Player Stun

--- CombatSystem/Player/States/PlayerStunnedState.cs
+++ CombatSystem/Player/States/PlayerStunnedState.cs
@@ -1,10 +1,14 @@
 ﻿using _Project.Systems._Core.StateMachine.Player;
+using _Project.Systems.MovementSystem.Player.States;
+using _Project.Systems.MovementSystem.Player.States.RootStates;
 
 namespace _Project.Systems.CombatSystem.Player.States
 {
     public class PlayerStunnedState : PlayerBaseState
     {
         private float stunTime;
+
+        private PlayerGroundedState GroundParent => GetSuperState() as PlayerGroundedState;
 
         public PlayerStunnedState(PlayerStateMachine stateMachine, float duration) : base(stateMachine)
         {
@@ -13,10 +17,17 @@ namespace _Project.Systems.CombatSystem.Player.States
 
         public override void Enter()
         {
+            stateMachine.Animator.CrossFadeInFixedTime(stateMachine.PlayerConfigSo.CombatData.StunnedAnimParamHash,
+                stateMachine.CrossFadeDuration);
         }
 
         public override void Tick(float deltaTime)
         {
+            stunTime -= deltaTime;
+            if (stunTime <= 0)
+            {
+                GroundParent?.SwitchSubState(new PlayerFreeLookState(stateMachine));
+            }
         }
 
         public override void Exit()

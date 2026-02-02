4fcaa63836420cffd1f9162320f9f730233026f9
BurakOzdemir <qburak.ozdemir.0640@gmail.com>
2026-02-03 02:56:30 +0300

    Advanced Observer pattern

---
+++ _Core/Observer/Observer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace _Project.Systems._Core.Observer
+{
+    [Serializable]
+    public class Observer<T>
+    {
+        [SerializeField] private T value;
+        [SerializeField] public UnityEvent<T> onValueChanged;
+
+        public T Value
+        {
+            get => value;
+            set => Set(value);
+        }
+
+        public Observer(T value, UnityAction<T> callback = null)
+        {
+            this.value = value;
+            onValueChanged = new UnityEvent<T>();
+            if (callback != null) onValueChanged.AddListener(callback);
+        }
+
+        public static implicit operator T(Observer<T> observer) => observer.Value;
+
+        public void Set(T value)
+        {
+            if (Equals(this.value, value)) return;
+            this.value = value;
+            onValueChanged.Invoke(value);
+        }
+
+        public void Invoke()
+        {
+            onValueChanged.Invoke(value);
+        }
+
+        public void AddListener(UnityAction<T> callback)
+        {
+            if (callback == null) return;
+            if (onValueChanged == null) onValueChanged = new UnityEvent<T>();
+
+            onValueChanged.AddListener(callback);
+        }
+
+        public void RemoveListener(UnityAction<T> callback)
+        {
+            if (callback == null) return;
+            if (onValueChanged == null) return;
+
+            onValueChanged.RemoveListener(callback);
+        }
+
+        public void RemoveAllListeners()
+        {
+            if (onValueChanged == null) return;
+
+            onValueChanged.RemoveAllListeners();
+        }
+
+        public void Dispose()
+        {
+            RemoveAllListeners();
+            onValueChanged = null;
+            value = default;
+        }
+    }
+}
